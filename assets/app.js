3dc7dd7b3e5c4ed94c75e3941e2243d1568c6f4e
Michael Rodriguez <mikeyrod22@gmail.com>
2021-10-21 14:57:32 -0500

    about section added, mobile nav now sticky, favicon changes dynamically

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,22 @@
 const root = document.documentElement;
+const favIcon = document.querySelector("link[rel='icon']");
 const lsKey = "ADVANCED-CSS-CHALLENGE-professional-portfolio-mikeyrod22";
 root.setAttribute("data-theme", localStorage.getItem(lsKey));
+
+localStorage.getItem(lsKey)
+  ? (favIcon.href = "./assets/favicon-light.ico")
+  : (favIcon.href = "./assets/favicon-dark.ico");
 
 function toggleTheme() {
   const lsTheme = localStorage.getItem(lsKey);
   if (lsTheme === "invert") {
     localStorage.setItem(lsKey, "");
     root.setAttribute("data-theme", "");
+    favIcon.href = "./assets/favicon-dark.ico";
   } else {
     localStorage.setItem(lsKey, "invert");
     root.setAttribute("data-theme", "invert");
+    favIcon.href = "./assets/favicon-light.ico";
   }
 }
 
